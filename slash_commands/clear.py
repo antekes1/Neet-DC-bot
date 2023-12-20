6d25c76140c2a3b3fc1c5c4e6bc652aa3f1ce121
Antek <99320445+antekes1@users.noreply.github.com>
2023-12-20 21:21:33 +0100

    add clear command

---
+++ slash_commands/clear.py
@@ -0,0 +1,28 @@
+import discord
+from discord import app_commands, Interaction, Member
+from discord.ext import commands
+from discord.ui import Button, View
+
+class clear(commands.Cog):
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @app_commands.command(name='clear', description="This command clearing messages")
+    @app_commands.default_permissions(manage_messages=True)
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def clear(self, interaction: discord.Interaction, count: int = None):
+        if count == None:
+            info = "all"
+        else:
+            info = int(count)
+
+        if count == None:
+            z = await interaction.channel.purge(limit=count)
+        else:
+           z = await interaction.channel.purge(limit=10000000000)
+        embed1 = discord.Embed(colour=discord.Colour.blue(), title=f"Clear {info}", description="Clearing ...")
+        await interaction.response.send_message(embed=embed1, ephemeral=True)
+
+async def setup(client: commands.Bot) -> None:
+    await client.add_cog(clear(client))
